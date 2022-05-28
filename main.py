cd2782607ea2c40e053c732df887a9ac647422bb
Ryan Raposo <raposo.ryan@gmail.com>
2022-05-28 06:54:00 -0400

    Change class structure Add cache to .gitignore Remove old generated images Add new entrypoint Suppress FutureWarnings

---
+++ main.py
@@ -0,0 +1,29 @@
+from hand import Hand
+
+
+if __name__ == '__main__':
+
+    hand = Hand()
+
+    lines = [
+        "Writer has written",
+        "One writes in kind",
+        "You see the hand",
+        "You see the mind",
+        "Source begets source",
+        "Begets source",
+        "Begets source"
+    ]
+    biases = [.75 for i in lines]
+    styles = [5 for i in lines]
+    stroke_colors = ['black' for i in lines]
+    stroke_widths = [1 for i in lines]
+    
+    hand.write(
+        filename='img/source.svg',
+        lines=lines,
+        biases=biases,
+        styles=styles,
+        stroke_colors=stroke_colors,
+        stroke_widths=stroke_widths
+    )
